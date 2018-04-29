3231d0d870126355e9ee8b366c62c7383d01cd03
Alberto Gonzalez <agonzalez@inventiaplus.com>
2018-04-29 08:39:00 +0100

    [feature]: completed RESTful service for User entity with pagination and sorting

--- src/main/java/es/edataconsulting/demo/resources/UserResources.java
+++ src/main/java/es/edataconsulting/demo/resources/UserResources.java
@@ -7,7 +7,10 @@ import java.util.Set;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.DefaultValue;
@@ -24,16 +27,12 @@ import javax.ws.rs.core.Response.Status;
 
 import es.edataconsulting.demo.model.Role;
 import es.edataconsulting.demo.model.User;
-import es.edataconsulting.demo.service.UserService;
-
 
 
 @Path("users")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class UserResources {
-	
-	UserService userService = new UserService();
 	
 	private EntityManager entityManager; 
 	
@@ -42,31 +41,42 @@ public class UserResources {
 		this.entityManager = entityManagerFactory.createEntityManager();
 	}
 
+	
+	//toDo: handle ORM exceptions
+	
+	
 	@GET
 	public List<User> getUsers(
-			@DefaultValue("10") @QueryParam("limit") long limit, 
-			@QueryParam("offset") long offset, 
-			@QueryParam("orderBy") List<String> orderBy) {
+			@DefaultValue("10") @QueryParam("limit") int limit, 
+			@QueryParam("offset") int offset, 
+			@DefaultValue("id") @QueryParam("orderBy") String orderBy,
+			@DefaultValue("asc") @QueryParam("orderDirection") String orderDirection) {
 		
-		Query q = entityManager.createQuery("SELECT u from User u");
-		@SuppressWarnings("unchecked")
-		List<User> users = q.getResultList();
-	    return users;
-		//return this.userService.getAllUsers();
+		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+		CriteriaQuery<User> criteria = cb.createQuery(User.class);
+		Root<User> root = criteria.from(User.class);
+		criteria.select(root);
+		if (orderDirection.equals("desc")) {
+			criteria.orderBy(cb.desc(root.get(orderBy)));
+		}else{
+			criteria.orderBy(cb.asc(root.get(orderBy)));
+		}
+		TypedQuery<User> q = entityManager.createQuery(criteria);
+		q.setFirstResult(offset);
+		q.setMaxResults(limit);
+		
+		return q.getResultList();
 	}
 	
 	@GET
 	@Path("/{userId}")
 	public User getUser(@PathParam("userId") long userId) {
-		if (userId > 10)
-			throw new RuntimeException("erro de prueba");
-		return new User(userId, "random user");
+		User user = entityManager.find(User.class, userId);
+		return user;
 	}
 	
 	@POST
 	public Response addUser(User user) {
-		//toDo: handle ORM exceptions
-		
 		entityManager.getTransaction().begin();		
 		if (user.getRoles().isEmpty()) {
 			Set<Role> roleSet = new HashSet<>();
@@ -75,7 +85,6 @@ public class UserResources {
 		}
 		entityManager.persist(user);
 		entityManager.getTransaction().commit();
-	      
 		return Response.status(Status.CREATED)
 				.entity(user)
 				.build();
@@ -83,13 +92,19 @@ public class UserResources {
 	
 	@PUT
 	public User updateUser(User user) {
+		entityManager.getTransaction().begin();		
+		entityManager.merge(user);
+		entityManager.getTransaction().commit();
 		return user;
 	}
 	
 	@DELETE
 	@Path("/{userId}")
 	public void deleteUser(@PathParam("userId") long userId) {
-
+		User user = entityManager.find(User.class, userId);
+		entityManager.getTransaction().begin();		
+		entityManager.remove(user);
+		entityManager.getTransaction().commit();
 	}
 	
 	@Path("/{userId}/roles")
